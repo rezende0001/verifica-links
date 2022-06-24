252c624af0bb1eb3cdcd1ed73abd9a0be8c69075
hype589 <lucascarmo589@gmail.com>
2022-06-24 19:08:20 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const { URL } = require('url');
+
+const https = require('./lib/check');
+
+
+function checkLink(link, callback) {
+    https.check(link, callback);
+}
+
+module.exports = checkLink
